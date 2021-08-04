1af00739405cd0cacaf6418bea8905ea18b82f94
Jake Epps <Molniz-95@yandex.ru>
2021-08-04 07:01:06 +0300

    nothing

--- Tests/JsonSpanParserTests/ParseTest.cs
+++ Tests/JsonSpanParserTests/ParseTest.cs
@@ -1,18 +1,32 @@
 using NUnit.Framework;
+using SpanParser.Json;
+using System.IO;
+using System;
 
 namespace JsonSpanParserTests
 {
     public class ParseTest
     {
+        JsonMemoryContext context;
+        char[] json;
         [SetUp]
         public void Setup()
         {
-
+            context = new JsonMemoryContext();
+            json = File.ReadAllText(@"C:\Users\Molni\source\repos\JsonSpanParser\Tests\message.json").ToCharArray();
         }
 
         [Test]
         public void Test1()
         {
+            try
+            {
+                var result = JSpan.Parse(json, context);
+            }
+            catch(Exception e)
+            {
+                Assert.Fail(e.Message);
+            }
             Assert.Pass();
         }
     }
