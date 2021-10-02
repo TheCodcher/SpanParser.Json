a6e751149cc06e2823745e8614c2bb683f218d02
Jake Epps <Molniz-95@yandex.ru>
2021-10-02 10:07:51 +0300

    add benchmark project

---
+++ Benchmark/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using BenchmarkDotNet.Running;
+
+namespace JsonSpanParserBenchmark
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            BenchmarkRunner.Run<JsonSpanParserLinqShortRun>();
+            BenchmarkRunner.Run<NewtonsoftJsonLinqShortRun>();
+
+            BenchmarkRunner.Run<JsonSpanParserPoolingRun>();
+            BenchmarkRunner.Run<NewtonsoftJsonPoolingRun>();
+        }
+    }
+}
